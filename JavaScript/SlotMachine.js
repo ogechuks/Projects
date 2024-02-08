259d61d5fe97d7cd1557d566782c269f830cc185
ogechuks <chkoge001@myuct.ac.za>
2024-02-08 19:31:10 +0200

    Added the getBet function aswell as added comments to the code.

--- JavaScript/SlotMachine.js
+++ JavaScript/SlotMachine.js
@@ -1,5 +1,12 @@
+/*
+Ogechukwu Chuks
+8 February 2024
+A terminal-based SlotMachine emulator 
+*/
+
 const prompt = require("prompt-sync")();
 
+//A function that is used to deposit money into the slotmachine before bets are made
 const deposit = () => {
     let depositAmount = prompt("Enter a deposit amount: ");
         let floatDepositAmount = parseFloat(depositAmount);
@@ -16,6 +23,7 @@ const deposit = () => {
     }
 }
 
+//A function used to get the number of lines the user will want to bet on
 const getNumberOfLines = () => {
     let lines = prompt("Enter the number of lines you would like to bet on: ");
     let NumberOfLines = parseFloat(lines);
@@ -33,5 +41,23 @@ const getNumberOfLines = () => {
 
 }
 
-const depositAmount = deposit();
-const lines = getNumberOfLines();
+//A function used to get the amount that the user will be betting
+const getBet = (balance, NumberOfLines) => {
+    let bet = prompt("Enter the bet amount per line: ");
+    let betAmount = parseFloat(bet) * NumberOfLines;
+
+    if (isNaN(betAmount) || betAmount < 0 || betAmount > balance/NumberOfLines){
+        while (isNaN(betAmount) || betAmount < 0 || betAmount > balance/NumberOfLines){
+            bet = prompt("Please enter a valid bet amount: ");
+            betAmount = parseFloat(bet);
+        }
+        return betAmount;
+    }
+    else{ 
+        return betAmount;
+    }
+}
+
+let balance = deposit();
+const NumberOfLines = getNumberOfLines();
+const bet = getBet(balance, NumberOfLines);
